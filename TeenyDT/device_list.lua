20d14ff1ab12b6be5601e64abc91a30541bee92e
xtoolbox <xtoolbox@gmail.com>
2019-02-03 23:02:20 +0800

    Make code compatible for All STM32 devices

--- TeenyDT/device_list.lua
+++ TeenyDT/device_list.lua
@@ -2,6 +2,16 @@
 return {
     {name = "STM32F0xx",      maxEp = 7,   maxMem = 1024, },
     {name = "STM32F10x_FS",   maxEp = 7,   maxMem = 512,  },
+    {name = "STM32F10x_OTG",  maxEp = 4,   maxMem = 1280, },
+    {name = "STM32F2xx_FS",   maxEp = 4,   maxMem = 1280, },
+    {name = "STM32F2xx_HS",   maxEp = 6,   maxMem = 4096, },
+    {name = "STM32F4xx_FS",   maxEp = 4,   maxMem = 1280, },
+    {name = "STM32F303_BC",   maxEp = 7,   maxMem = 512,  },
+    {name = "STM32F373x",     maxEp = 7,   maxMem = 512,  },
+    {name = "STM32F303_DE",   maxEp = 7,   maxMem = 1024, },
+    {name = "STM32F4xx_HS",   maxEp = 6,   maxMem = 4096, },
     {name = "STM32F7xx_FS",   maxEp = 6,   maxMem = 1280, },
     {name = "STM32F7xx_HS",   maxEp = 9,   maxMem = 4096, },
+    {name = "STM32H7xx_FS",   maxEp = 9,   maxMem = 1280, },
+    {name = "STM32H7xx_HS",   maxEp = 9,   maxMem = 4096, },
 }
